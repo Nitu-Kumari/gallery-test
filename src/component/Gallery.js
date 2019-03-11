ee1de0b3c0cc5ce4dba3fa70ad0f067e99dc464c
nitu <nituk650@gmail.com>
2019-03-11 14:54:15 +0530

    done

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-
-
+import ModalImage from 'react-modal-image'
 
 class Gallery extends Component {
 constructor(){
 super();
+this.state={images:[]}
 this.handleSubmit=this.handleSubmit.bind(this);
 }
 
@@ -13,12 +13,13 @@ handleSubmit(event){
   event.preventDefault();
   const data=new FormData(event.target);
   fetch('http://localhost:7000/gallerys',{
-    method:'POST',
-    body:data,
+  method:'POST',
+  body:data,
+  }).then(response => response.json())
+  .then(data => this.setState({images: [...this.state.images, data]})); 
 
-}); 
-}
-  
+  }
+
   componentDidMount() {
     this.getAllImages();
   }
@@ -35,32 +36,41 @@ handleSubmit(event){
       })
   }
 
-  
   render() {
     return (
       <div>
         <div className="alert alert-success" role="alert">
         Welcome to Gallery App
-        </div>
-        <form  onSubmit={this.handleSubmit}>
-          <div>
-            <input type="file" name="gallery" id="gallery" />
+        </div> 
+        <form  onSubmit={this.handleSubmit}>  
+        <div>
+    <input type="file" name="gallery" id="gallery" />
             <br />
           </div>
-          <button type="submit"> Upload</button>
+          <button className="btn btn-primary" type="submit" >Upload</button>
         </form>
+    
         <div>
           <hr />
+          <div class="container">
+         <div class="row col-xs-2">
           {
             this.state.images.length ? this.state.images.map(image => {
               return (
-                <div>
-                  <img src={image.image} alt=""/>
-                </div>
+                       <div>
+                        <ModalImage
+                        className="image"   
+                        small={image.image}
+                         large={image.image}
+                          alt={image.name}
+                               />
+                           </div>
               )
-            }): "No images found"
+            }): "image"
           }
         </div>
+    </div>
+    </div>
     </div>
     );
   }
